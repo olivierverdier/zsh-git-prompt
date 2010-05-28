5951b18c22fc02f0bf2320a1c7d7fc82356411f5
Olivier Verdier <olivier.verdier@gmail.com>
2010-05-28 17:54:25 +0200

    fixed zsh splitting with empty parts

--- update_current_git_vars.sh
+++ update_current_git_vars.sh
@@ -2,4 +2,4 @@ unset __CURRENT_GIT_STATUS
 
 local gitstatus="${HOME}/.zsh/gitstatus.py"
 _GIT_STATUS=`python ${gitstatus}`
-__CURRENT_GIT_STATUS=(${(s/:/)_GIT_STATUS})
+__CURRENT_GIT_STATUS=("${(s/:/)_GIT_STATUS}")
