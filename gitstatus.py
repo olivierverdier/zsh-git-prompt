1a36e0ed3cab51291f5eb579953299cf85af5145
Olivier Verdier <olivier.verdier@gmail.com>
2010-05-28 16:34:24 +0200

    separate variable for relation to remote information

--- gitstatus.py
+++ gitstatus.py
@@ -29,6 +29,8 @@ if untracked.search(output):
 if status == '':
 	status = symbols['clean']
 
+remote = ''
+
 bline = lines[0]
 if bline.find('Not currently on any branch') != -1:
 	branch = symbols['sha1']+ Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0][:-1]
@@ -37,12 +39,12 @@ else:
 	bstatusline = lines[1]
 	match = behead_re.match(bstatusline)
 	if match:
-		branch += symbols[match.groups()[0]]
-		branch += match.groups()[2]
+		remote = symbols[match.groups()[0]]
+		remote += match.groups()[2]
 	elif lines[2:]:
 		div_match = diverge_re.match(lines[2])
 	 	if div_match:
-			branch += "{behind}{1}{ahead of}{0}".format(*div_match.groups(), **symbols)
+			remote = "{behind}{1}{ahead of}{0}".format(*div_match.groups(), **symbols)
 
-print '(%s%s)' % (branch,status)
+print '(%s%s%s)' % (branch,remote,status)
 
