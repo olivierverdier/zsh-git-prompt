fabdbc7bf5e8fab68e6cb48a23cec3ecf17b305c
Jason Axelson <jason.axelson@gmail.com>
2010-08-01 16:04:52 -1000

    Fix to make work on my version of zsh

--- zshrc.sh
+++ zshrc.sh
@@ -12,16 +12,19 @@ setopt PROMPT_SUBST
 # Autoload zsh functions.
 fpath=($__GIT_PROMPT_DIR/functions $fpath)
 autoload -U $__GIT_PROMPT_DIR/functions/*(:t)
+
+function preexec() {
+    preexec_update_git_vars
+}
+
+function precmd() {
+    precmd_update_git_vars
+}
+
+function chpwd() {
+    chpwd_update_git_vars
+}
  
-# Enable auto-execution of functions.
-typeset -ga preexec_functions
-typeset -ga precmd_functions
-typeset -ga chpwd_functions
- 
-# Append git functions needed for prompt.
-preexec_functions+='preexec_update_git_vars'
-precmd_functions+='precmd_update_git_vars'
-chpwd_functions+='chpwd_update_git_vars'
 
 # Set the prompt.
 PROMPT='%B%m%~%b$(prompt_git_info) %# '
