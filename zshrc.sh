8619987518773727b6a22bf6996207517fada043
John Buttery <john.buttery@rackspace.com>
2013-08-27 14:52:27 -0500

    Add tag to the prompt
    By default, the tag will only be shown if it points directly to the
    current HEAD; a configuration option is provided to allow showing the
    tag information at all times (when no tag points directly to HEAD, the
    closest "parent tag" as determined by "git describe" will be shown).

--- zshrc.sh
+++ zshrc.sh
@@ -53,12 +53,27 @@ function update_current_git_vars() {
 	GIT_CLEAN=$__CURRENT_GIT_STATUS[7]
 }
 
+function update_current_git_tag() {
+    ## Get the actual tag from git; either an exact match or not
+    if [ ${ZSH_GIT_PROMPT_ALWAYS_SHOW_TAG} -eq 1 ]; then
+        GIT_CURRENT_TAG=`git describe --tags 2>/dev/null`
+    else
+        GIT_CURRENT_TAG=`git describe --tags --exact-match 2>/dev/null`
+    fi
+    ## If we got a tag back, dress it up for placement in the prompt
+    if [ ! -z ${GIT_CURRENT_TAG} ]; then
+        GIT_TAG_STATUS="${ZSH_THEME_GIT_PROMPT_TAG_PREFIX}${GIT_CURRENT_TAG}${ZSH_THEME_GIT_PROMPT_TAG_SUFFIX}"
+    else
+        GIT_TAG_STATUS=""
+    fi
+}
+
 
 git_super_status() {
 	precmd_update_git_vars
     if [ -n "$__CURRENT_GIT_STATUS" ]; then
-	  STATUS="($GIT_BRANCH"
-	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
+	  update_current_git_tag
+	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH$GIT_TAG_STATUS%{${reset_color}%}"
 	  if [ -n "$GIT_REMOTE" ]; then
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_REMOTE$GIT_REMOTE%{${reset_color}%}"
 	  fi
@@ -96,6 +111,9 @@ ZSH_THEME_GIT_PROMPT_REMOTE=""
 ZSH_THEME_GIT_PROMPT_UNTRACKED="…"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}✔"
 
+ZSH_GIT_PROMPT_ALWAYS_SHOW_TAG=0
+ZSH_THEME_GIT_PROMPT_TAG_PREFIX="("
+ZSH_THEME_GIT_PROMPT_TAG_SUFFIX=")"
 
 # Use local configuration file if it exists
 [ -e "${__LOCAL_CONFIGURATION_FILE}" ] && . "${__LOCAL_CONFIGURATION_FILE}"
