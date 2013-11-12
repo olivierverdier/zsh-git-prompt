cb628eb14bdbe4fb3312d92ff92345a705899438
Olivier Verdier <olivier.verdier@gmail.com>
2013-11-12 11:33:27 +0100

    add stg to triggering command list

--- zshrc.sh
+++ zshrc.sh
@@ -18,7 +18,7 @@ add-zsh-hook precmd precmd_update_git_vars
 ## Function definitions
 function preexec_update_git_vars() {
     case "$2" in
-        git*|hub*|gh*)
+        git*|hub*|gh*|stg*)
         __EXECUTED_GIT_COMMAND=1
         ;;
     esac
