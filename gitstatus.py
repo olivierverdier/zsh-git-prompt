f651a1ebe56f590dd3cd78e0d85a553d0721fd2e
aardvark <aardvark@bk.ru>
2013-11-25 10:01:31 +0300

    fixed error with not branch in some terminals
    in some cases and terminals checkouting certain commit will issue error of non implicit byte-string conversion
    explicit decoding fixes this problem

--- gitstatus.py
+++ gitstatus.py
@@ -41,7 +41,7 @@ else:
 remote = ''
 
 if not branch: # not on any branch
-	branch = symbols['prehash']+ Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0][:-1]
+	branch = symbols['prehash']+ Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0].decode('utf-8')[:-1]
 else:
 	remote_name = Popen(['git','config','branch.%s.remote' % branch], stdout=PIPE).communicate()[0].strip()
 	if remote_name:
