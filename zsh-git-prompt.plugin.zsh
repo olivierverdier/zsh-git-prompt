76a7c864d21dc7be78267897ef5816bf49ea50e4
knakayama <knakayama.sh@gmail.com>
2015-06-16 16:37:00 +0900

    Revert "unko"
    This reverts commit 6a61c70926b3d6458ec070543cb7db5edfad5070.

--- zsh-git-prompt.plugin.zsh
+++ zsh-git-prompt.plugin.zsh
@@ -1,17 +1,21 @@
 export __GIT_PROMPT_DIR=${0:A:h}
+
 export GIT_PROMPT_EXECUTABLE=${GIT_PROMPT_USE_PYTHON:-"python"}
 
 # Initialize colors.
-autoload -U colors && colors
+autoload -U colors
+colors
 
 # Allow for functions in the prompt.
 setopt PROMPT_SUBST
 
 autoload -U add-zsh-hook
+
 add-zsh-hook chpwd   -chpwd-update-git-vars
 add-zsh-hook preexec -preexec-update-git-vars
 add-zsh-hook precmd  -precmd-update-git-vars
 
+## Function definitions
 -preexec-update-git-vars() {
   case "$2" in
     git*|hub*|gh*|stg*)
@@ -21,7 +25,7 @@ add-zsh-hook precmd  -precmd-update-git-vars
 }
 
 -precmd-update-git-vars() {
-  if [[ -n "$__EXECUTED_GIT_COMMAND" || -z "$ZSH_GP_CACHE" ]]; then
+  if [[ -n "$__EXECUTED_GIT_COMMAND" || ! -n "$ZSH_GP_CACHE" ]]; then
     -update-current-git-vars
     unset __EXECUTED_GIT_COMMAND
   fi
@@ -36,7 +40,7 @@ add-zsh-hook precmd  -precmd-update-git-vars
 
   if [[ "$GIT_PROMPT_EXECUTABLE" == "python" ]]; then
       local gitstatus="${__GIT_PROMPT_DIR}/gitstatus.py"
-      local _GIT_STATUS=`python ${gitstatus} 2>/dev/null`
+      _GIT_STATUS=`python ${gitstatus} 2>/dev/null`
   fi
 
   __CURRENT_GIT_STATUS=("${(@s: :)_GIT_STATUS}")
@@ -52,33 +56,32 @@ add-zsh-hook precmd  -precmd-update-git-vars
 
 -git-super-status() {
   -precmd-update-git-vars
-  local status
-  if [[ -n "$__CURRENT_GIT_status" ]]; then
-    status="${ZSH_GP_PREFIX}${ZSH_GP_YELLOW}${GIT_BRANCH}%{${reset_color}%}"
+  if [[ -n "$__CURRENT_GIT_STATUS" ]]; then
+    STATUS="${ZSH_GP_PREFIX}${ZSH_GP_YELLOW}${GIT_BRANCH}%{${reset_color}%}"
     if [[ $GIT_BEHIND -ne 0 ]]; then
-      status="${status}${ZSH_GP_BASIC_COLOR}${ZSH_GP_BEHIND}${GIT_BEHIND}%{${reset_color}%}"
+      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_BEHIND}${GIT_BEHIND}%{${reset_color}%}"
     fi
     if [[ $GIT_AHEAD -ne 0 ]]; then
-      status="${status}${ZSH_GP_BASIC_COLOR}${ZSH_GP_AHEAD}${GIT_AHEAD}%{${reset_color}%}"
+      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_AHEAD}${GIT_AHEAD}%{${reset_color}%}"
     fi
-    status="${status}${ZSH_GP_SEPARATOR}"
+    STATUS="${STATUS}${ZSH_GP_SEPARATOR}"
     if [[ $GIT_STAGED -ne 0 ]]; then
-      status="${status}${ZSH_GP_BASIC_COLOR}${ZSH_GP_STAGED}${GIT_STAGED}%{${reset_color}%}"
+      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_STAGED}${GIT_STAGED}%{${reset_color}%}"
     fi
     if [[ $GIT_CONFLICTS -ne 0 ]]; then
-      status="${status}${ZSH_GP_BASIC_COLOR}${ZSH_GP_CONFLICTS}${GIT_CONFLICTS}%{${reset_color}%}"
+      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_CONFLICTS}${GIT_CONFLICTS}%{${reset_color}%}"
     fi
     if [[ $GIT_CHANGED -ne 0 ]]; then
-      status="${status}${ZSH_GP_BASIC_COLOR}${ZSH_GP_CHANGED}${GIT_CHANGED}%{${reset_color}%}"
+      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_CHANGED}${GIT_CHANGED}%{${reset_color}%}"
     fi
     if [[ $GIT_UNTRACKED -ne 0 ]]; then
-      status="${status}${ZSH_GP_BASIC_COLOR}${ZSH_GP_UNTRACKED}${GIT_UNTRACKED}%{${reset_color}%}"
+      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_UNTRACKED}%{${reset_color}%}"
     fi
     if [[ $GIT_CHANGED -eq 0 && $GIT_CONFLICTS -eq 0 && $GIT_STAGED -eq 0 && $GIT_UNTRACKED -eq 0 ]]; then
-      status="${status}${ZSH_GP_GREEN}${ZSH_GP_CLEAN}"
+      STATUS="${STATUS}${ZSH_GP_GREEN}${ZSH_GP_CLEAN}"
     fi
-    status="${status}%{${reset_color}%}${ZSH_GP_SUFFIX}"
-    echo "$status"
+    STATUS="${STATUS}%{${reset_color}%}${ZSH_GP_SUFFIX}"
+    echo "$STATUS"
   fi
 }
 
@@ -90,7 +93,7 @@ ZSH_GP_BASIC_COLOR="%{$fg[red]%}%{%G%}"
 ZSH_GP_YELLOW="%{$fg_bold[yellow]%}"
 ZSH_GP_GREEN="%{$fg_bold[green]%}%{%G%}"
 ZSH_GP_STAGED="A"
-ZSH_GP_CONFLICTS="UU"
+ZSH_GP_CONFLICTS=" unmerged"
 ZSH_GP_CHANGED="M"
 ZSH_GP_AHEAD=">"
 ZSH_GP_BEHIND="<"
