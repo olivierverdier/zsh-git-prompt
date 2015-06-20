81b8c69e7b55dcff020048f91ed4d402782ed301
knakayama <knakayama.sh@gmail.com>
2015-06-20 22:37:07 +0900

    add _ to each variable

--- zsh-git-prompt.plugin.zsh
+++ zsh-git-prompt.plugin.zsh
@@ -1,5 +1,5 @@
-export __GIT_PROMPT_DIR=${0:A:h}
-export GIT_PROMPT_EXECUTABLE=${GIT_PROMPT_USE_PYTHON:-"python"}
+export _GIT_PROMPT_DIR=${0:A:h}
+export _GIT_PROMPT_EXECUTABLE=${_GIT_PROMPT_USE_PYTHON:-"python"}
 
 # Initialize colors.
 autoload -U colors && colors
@@ -16,15 +16,15 @@ add-zsh-hook precmd  -precmd-update-git-vars
 -preexec-update-git-vars() {
   case "$2" in
     git*|hub*|gh*|stg*)
-    __EXECUTED_GIT_COMMAND=1
+    _EXECUTED_GIT_COMMAND=1
     ;;
   esac
 }
 
 -precmd-update-git-vars() {
-  if [[ -n "$__EXECUTED_GIT_COMMAND" || -z "$ZSH_GP_CACHE" ]]; then
+  if [[ -n "$_EXECUTED_GIT_COMMAND" ]]; then
     -update-current-git-vars
-    unset __EXECUTED_GIT_COMMAND
+    unset _EXECUTED_GIT_COMMAND
   fi
 }
 
@@ -33,69 +33,69 @@ add-zsh-hook precmd  -precmd-update-git-vars
 }
 
 -update-current-git-vars() {
-  unset __CURRENT_GIT_STATUS
+  unset _CURRENT_GIT_STATUS
 
-  if [[ "$GIT_PROMPT_EXECUTABLE" == "python" ]]; then
-      local gitstatus="${__GIT_PROMPT_DIR}/gitstatus.py"
+  if [[ "$_GIT_PROMPT_EXECUTABLE" == "python" ]]; then
+      local gitstatus="${_GIT_PROMPT_DIR}/gitstatus.py"
       _GIT_STATUS=`python ${gitstatus} 2>/dev/null`
   fi
 
-  __CURRENT_GIT_STATUS=("${(@s: :)_GIT_STATUS}")
-  GIT_BRANCH=$__CURRENT_GIT_STATUS[1]
-  GIT_AHEAD=$__CURRENT_GIT_STATUS[2]
-  GIT_BEHIND=$__CURRENT_GIT_STATUS[3]
-  GIT_STAGED=$__CURRENT_GIT_STATUS[4]
-  GIT_CONFLICTS=$__CURRENT_GIT_STATUS[5]
-  GIT_CHANGED=$__CURRENT_GIT_STATUS[6]
-  GIT_UNTRACKED=$__CURRENT_GIT_STATUS[7]
+  _CURRENT_GIT_STATUS=("${(@s: :)_GIT_STATUS}")
+  _GIT_BRANCH=$_CURRENT_GIT_STATUS[1]
+  _GIT_AHEAD=$_CURRENT_GIT_STATUS[2]
+  _GIT_BEHIND=$_CURRENT_GIT_STATUS[3]
+  _GIT_STAGED=$_CURRENT_GIT_STATUS[4]
+  _GIT_CONFLICTS=$_CURRENT_GIT_STATUS[5]
+  _GIT_CHANGED=$_CURRENT_GIT_STATUS[6]
+  _GIT_UNTRACKED=$_CURRENT_GIT_STATUS[7]
 }
 
 
 -git-super-status() {
   -precmd-update-git-vars
-  if [[ -n "$__CURRENT_GIT_STATUS" ]]; then
-    STATUS="${ZSH_GP_PREFIX}${ZSH_GP_YELLOW}${GIT_BRANCH}%{${reset_color}%}"
-    if [[ $GIT_BEHIND -ne 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_BEHIND}${GIT_BEHIND}%{${reset_color}%}"
+  if [[ -n "$_CURRENT_GIT_STATUS" ]]; then
+    _STATUS="${_ZSH_GP_PREFIX}${_ZSH_GP_YELLOW}${_GIT_BRANCH}%{${reset_color}%}"
+    if [[ $_GIT_BEHIND -ne 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_BASIC_COLOR}${_ZSH_GP_BEHIND}${_GIT_BEHIND}%{${reset_color}%}"
     fi
-    if [[ $GIT_AHEAD -ne 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_AHEAD}${GIT_AHEAD}%{${reset_color}%}"
+    if [[ $_GIT_AHEAD -ne 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_BASIC_COLOR}${_ZSH_GP_AHEAD}${_GIT_AHEAD}%{${reset_color}%}"
     fi
-    STATUS="${STATUS}${ZSH_GP_SEPARATOR}"
-    if [[ $GIT_STAGED -ne 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_STAGED}${GIT_STAGED}%{${reset_color}%}"
+    _STATUS="${_STATUS}${_ZSH_GP_SEPARATOR}"
+    if [[ $_GIT_STAGED -ne 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_BASIC_COLOR}${_ZSH_GP_STAGED}${_GIT_STAGED}%{${reset_color}%}"
     fi
-    if [[ $GIT_CONFLICTS -ne 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_CONFLICTS}${GIT_CONFLICTS}%{${reset_color}%}"
+    if [[ $_GIT_CONFLICTS -ne 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_BASIC_COLOR}${_ZSH_GP_CONFLICTS}${_GIT_CONFLICTS}%{${reset_color}%}"
     fi
-    if [[ $GIT_CHANGED -ne 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_CHANGED}${GIT_CHANGED}%{${reset_color}%}"
+    if [[ $_GIT_CHANGED -ne 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_BASIC_COLOR}${_ZSH_GP_CHANGED}${_GIT_CHANGED}%{${reset_color}%}"
     fi
-    if [[ $GIT_UNTRACKED -ne 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_BASIC_COLOR}${ZSH_GP_UNTRACKED}${GIT_UNTRACKED}%{${reset_color}%}"
+    if [[ $_GIT_UNTRACKED -ne 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_BASIC_COLOR}${_ZSH_GP_UNTRACKED}${_GIT_UNTRACKED}%{${reset_color}%}"
     fi
-    if [[ $GIT_CHANGED -eq 0 && $GIT_CONFLICTS -eq 0 && $GIT_STAGED -eq 0 && $GIT_UNTRACKED -eq 0 ]]; then
-      STATUS="${STATUS}${ZSH_GP_GREEN}${ZSH_GP_CLEAN}"
+    if [[ $_GIT_CHANGED -eq 0 && $_GIT_CONFLICTS -eq 0 && $_GIT_STAGED -eq 0 && $_GIT_UNTRACKED -eq 0 ]]; then
+      _STATUS="${_STATUS}${_ZSH_GP_GREEN}${_ZSH_GP_CLEAN}"
     fi
-    STATUS="${STATUS}%{${reset_color}%}${ZSH_GP_SUFFIX}"
-    echo "$STATUS"
+    _STATUS="${_STATUS}%{${reset_color}%}${_ZSH_GP_SUFFIX}"
+    echo "$_STATUS"
   fi
 }
 
 # Default values for the appearance of the prompt. Configure at will.
-ZSH_GP_PREFIX="("
-ZSH_GP_SUFFIX=")"
-ZSH_GP_SEPARATOR="|"
-ZSH_GP_BASIC_COLOR="%{$fg_bold[red]%}%{%G%}"
-ZSH_GP_YELLOW="%{$fg_bold[yellow]%}"
-ZSH_GP_GREEN="%{$fg_bold[green]%}%{%G%}"
-ZSH_GP_STAGED="A"
-ZSH_GP_CONFLICTS="UU"
-ZSH_GP_CHANGED="M"
-ZSH_GP_AHEAD=">"
-ZSH_GP_BEHIND="<"
-ZSH_GP_UNTRACKED="??"
-ZSH_GP_CLEAN="clean"
+_ZSH_GP_PREFIX="("
+_ZSH_GP_SUFFIX=")"
+_ZSH_GP_SEPARATOR="|"
+_ZSH_GP_BASIC_COLOR="%{$fg_bold[red]%}%{%G%}"
+_ZSH_GP_YELLOW="%{$fg_bold[yellow]%}"
+_ZSH_GP_GREEN="%{$fg_bold[green]%}%{%G%}"
+_ZSH_GP_STAGED="A"
+_ZSH_GP_CONFLICTS="UU"
+_ZSH_GP_CHANGED="M"
+_ZSH_GP_AHEAD=">"
+_ZSH_GP_BEHIND="<"
+_ZSH_GP_UNTRACKED="??"
+_ZSH_GP_CLEAN="clean"
 
 # Local Variables:
 # mode: Shell-Script
