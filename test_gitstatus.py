3e6db1a3763ebbd5538ef5dfd58726bc7940d073
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2018-05-26 15:04:18 -0400

    Working tests for conflicts and remote.

--- test_gitstatus.py
+++ test_gitstatus.py
@@ -118,6 +118,73 @@ def git_repo_compute_stats():
         os.chdir(cwd)
 
 
+def git_repo_compute_stats_only_conflicts():
+    """
+    Create a fake git repo with the following properties:
+        - upstream set to another local git repo
+        - edit the same file and create a merge conflict
+    """
+    cwd = os.getcwd()
+    folder = "/tmp/zsh_git"
+    folder_up = folder + "_upstream"
+    cmds = [
+        "git init",
+        "first:A single line\nsecond line\third line",
+        "git add first",
+        "git commit -m 'first commit'",
+        "first:fourth line\nfifth line\n",
+        "git add first",
+        "git commit -m 'second commit'",
+        "cp -R %s %s" % (folder, folder_up),
+        "git reset --hard HEAD~1",
+        "first:ninth line\ntenth line\n",
+        "git add first",
+        "git commit -m 'new second commit'",
+        "git remote add -f up %s" % folder_up,
+        "git branch --set-upstream-to=up/master",
+        "git fetch up",
+        "git merge up/master",
+    ]
+    try:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.makedirs(folder)
+        os.chdir(folder)
+
+        for cmd in cmds:
+            if re.match(r'\S+:', cmd):
+                assert len(cmd.split(":")) == 2
+                fname, text = cmd.split(":")
+                with open(os.path.join(folder, fname), 'a') as fout:
+                    fout.write(text + '\n')
+            else:
+                try:
+                    with open(os.devnull, 'w') as devnull:
+                        subprocess.check_call(shlex.split(cmd),
+                                              stdout=devnull, stderr=subprocess.STDOUT)
+                except subprocess.CalledProcessError:
+                    pass
+
+        yield
+
+    finally:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.chdir(cwd)
+
+
 def git_repo_branch_on_hash():
     """
     Create a fake git repo with the following properties:
@@ -211,18 +278,195 @@ def git_repo_branch_on_master():
         os.chdir(cwd)
 
 
-# TODO: Use porcelain for status, saves two commands
-#  conflicts = 0
-#  untracked = 0
-#  staged = 0
-#  changed = 0
-#  for line in run_cmd(['git', 'status', '--porcelain']).splitlines():
-    #  if line[0] in ['A', 'M']:
-        #  changed += 1
-    #  if line[0] in ['A', 'M']:
-        #  staged += 1
-    #  if line[0:2] == '??':
-        #  untracked += 1
+def git_repo_remote_ahead():
+    """
+    Create a fake git repo with the following properties:
+        - main repo has 3 commits
+        - upstream repo has 2 commits
+        - main repo has upstream set and is AHEAD by 1 commit
+    """
+    cwd = os.getcwd()
+    folder = "/tmp/zsh_git"
+    folder_up = folder + "_upstream"
+    cmds = [
+        "git init",
+        "first:A single line",
+        "git add first",
+        "git commit -m 'first commit'",
+        "first:Second line",
+        "git add first",
+        "git commit -m 'second commit'",
+        "cp -R %s %s" % (folder, folder_up),
+        "first:third line",
+        "git add first",
+        "git commit -m 'third commit'",
+        "git remote add -f up %s" % folder_up,
+        "git branch --set-upstream-to=up/master",
+    ]
+    try:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.makedirs(folder)
+        os.chdir(folder)
+
+        for cmd in cmds:
+            if re.match(r'\S+:', cmd):
+                assert len(cmd.split(":")) == 2
+                fname, text = cmd.split(":")
+                with open(os.path.join(folder, fname), 'a') as fout:
+                    fout.write(text + '\n')
+            else:
+                with open(os.devnull, 'w') as devnull:
+                    subprocess.check_call(shlex.split(cmd),
+                                          stdout=devnull, stderr=subprocess.STDOUT)
+
+        yield
+
+    finally:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.chdir(cwd)
+
+
+def git_repo_remote_behind():
+    """
+    Create a fake git repo with the following properties:
+        - main repo has 2 commits
+        - upstream repo has 3 commits
+        - main repo has upstream set and is BEHIND by 1 commit
+    """
+    cwd = os.getcwd()
+    folder = "/tmp/zsh_git"
+    folder_up = folder + "_upstream"
+    cmds = [
+        "git init",
+        "first:A single line",
+        "git add first",
+        "git commit -m 'first commit'",
+        "first:Second line",
+        "git add first",
+        "git commit -m 'second commit'",
+        "first:third line",
+        "git add first",
+        "git commit -m 'third commit'",
+        "cp -R %s %s" % (folder, folder_up),
+        "git remote add -f up %s" % folder_up,
+        "git branch --set-upstream-to=up/master",
+        "git reset --hard HEAD~1",
+    ]
+    try:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.makedirs(folder)
+        os.chdir(folder)
+
+        for cmd in cmds:
+            if re.match(r'\S+:', cmd):
+                assert len(cmd.split(":")) == 2
+                fname, text = cmd.split(":")
+                with open(os.path.join(folder, fname), 'a') as fout:
+                    fout.write(text + '\n')
+            else:
+                with open(os.devnull, 'w') as devnull:
+                    subprocess.check_call(shlex.split(cmd),
+                                          stdout=devnull, stderr=subprocess.STDOUT)
+
+        yield
+
+    finally:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.chdir(cwd)
+
+
+def git_repo_remote_diverged():
+    """
+    Create a fake git repo with the following properties:
+        - main repo has 3 commits
+        - upstream repo has 3 commits
+        - main repo has upstream set and is has diverged by 1 commit each way
+    """
+    cwd = os.getcwd()
+    folder = "/tmp/zsh_git"
+    folder_up = folder + "_upstream"
+    cmds = [
+        "git init",
+        "first:A single line",
+        "git add first",
+        "git commit -m 'first commit'",
+        "first:Second line",
+        "git add first",
+        "git commit -m 'second commit'",
+        "first:third line",
+        "git add first",
+        "git commit -m 'third commit'",
+        "cp -R %s %s" % (folder, folder_up),
+        "git remote add -f up %s" % folder_up,
+        "git branch --set-upstream-to=up/master",
+        "git reset --hard HEAD~1",
+        "first:different third line",
+        "git add first",
+        "git commit -m 'different third commit'",
+    ]
+    try:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.makedirs(folder)
+        os.chdir(folder)
+
+        for cmd in cmds:
+            if re.match(r'\S+:', cmd):
+                assert len(cmd.split(":")) == 2
+                fname, text = cmd.split(":")
+                with open(os.path.join(folder, fname), 'a') as fout:
+                    fout.write(text + '\n')
+            else:
+                with open(os.devnull, 'w') as devnull:
+                    subprocess.check_call(shlex.split(cmd),
+                                          stdout=devnull, stderr=subprocess.STDOUT)
+
+        yield
+
+    finally:
+        try:
+            shutil.rmtree(folder)
+        except (OSError, IOError):
+            pass
+        try:
+            shutil.rmtree(folder_up)
+        except (OSError, IOError):
+            pass
+        os.chdir(cwd)
 
 
 def test_run_cmd():
@@ -267,3 +511,23 @@ def test_branch_hash():
 @decorate_test(git_repo_compute_stats)
 def test_compute_stats_no_conflicts():
     assert run_gitstatus() == 'master 0 0 3 0 1 2 1'
+
+
+@decorate_test(git_repo_compute_stats_only_conflicts)
+def test_compute_stats_only_conflicts():
+    assert run_gitstatus() == 'master 1 1 0 1 1 0 0'
+
+
+@decorate_test(git_repo_remote_ahead)
+def test_remote_ahead():
+    assert run_gitstatus() == 'master 0 1 0 0 0 0 0'
+
+
+@decorate_test(git_repo_remote_behind)
+def test_remote_behind():
+    assert run_gitstatus() == 'master 1 0 0 0 0 0 0'
+
+
+@decorate_test(git_repo_remote_diverged)
+def test_remote_diverged():
+    assert run_gitstatus() == 'master 1 1 0 0 0 0 0'
