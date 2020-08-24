8e297310621eb55f5491a72faec850da5eefdda0
Yogesh V <yogesh@Yogeshs-MacBook-Pro.local>
2020-08-24 15:49:20 +0530

    show branch details only when directory is in a git repo

--- zshrc.sh
+++ zshrc.sh
@@ -63,7 +63,7 @@ function update_current_git_vars() {
 
 git_super_status() {
 	precmd_update_git_vars
-    if [ -n "$__CURRENT_GIT_STATUS" ]; then
+    if [ -n "$__CURRENT_GIT_STATUS" ] && [ "$GIT_BRANCH" != ":" ]; then
 	  STATUS="$ZSH_THEME_GIT_PROMPT_PREFIX$ZSH_THEME_GIT_PROMPT_BRANCH$GIT_BRANCH%{${reset_color}%}"
 	  if [ "$GIT_BEHIND" -ne "0" ]; then
 		  STATUS="$STATUS$ZSH_THEME_GIT_PROMPT_BEHIND$GIT_BEHIND%{${reset_color}%}"
